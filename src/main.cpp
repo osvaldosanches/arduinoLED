ac6f28ad674195993949dca3efb621f10e1e113f
osvaldosanches <vado.benites@gmail.com>
2019-12-15 13:27:33 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//Osvaldo Sanches
+//Teste de funcionamento de arduino com led
+
+#include <Arduino.h>
+
+int led = 8; // led conectado no pino 8 - positivo
+
+void setup() {
+
+  pinMode (led, OUTPUT);  // Pino 8 será saída na declaração de função
+  
+}
+
+void loop() {
+
+  digitalWrite (led, HIGH); // LED ligado
+  
+  delay (5000); // Aguarda 5 segundos
+  
+  digitalWrite (led, LOW); // LED desligado
+  
+  delay (5000); // Aguarda 5 segundos
+  
+}
